4fc1aaa9ab44e75e72376a0ba50524745903a64d
moonyoungCHAE <xpf_fl@naver.com>
2019-09-28 15:51:22 +0900

    Add Android Native

--- android/src/main/java/flutter/moum/open_appstore/OpenAppstorePlugin.java
+++ android/src/main/java/flutter/moum/open_appstore/OpenAppstorePlugin.java
@@ -1,5 +1,8 @@
 package flutter.moum.open_appstore;
 
+import android.content.Intent;
+import android.net.Uri;
+
 import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
@@ -9,16 +12,32 @@ import io.flutter.plugin.common.PluginRegistry.Registrar;
 /** OpenAppstorePlugin */
 public class OpenAppstorePlugin implements MethodCallHandler {
   /** Plugin registration. */
+  private final Registrar mRegistrar;
+
+  private OpenAppstorePlugin(Registrar registrar){
+    this.mRegistrar = registrar;
+  }
+
   public static void registerWith(Registrar registrar) {
     final MethodChannel channel = new MethodChannel(registrar.messenger(), "open_appstore");
-    channel.setMethodCallHandler(new OpenAppstorePlugin());
+    channel.setMethodCallHandler(new OpenAppstorePlugin(registrar));
   }
 
   @Override
   public void onMethodCall(MethodCall call, Result result) {
     if (call.method.equals("getPlatformVersion")) {
       result.success("Android " + android.os.Build.VERSION.RELEASE);
-    } else {
+    }
+    else if (call.method.equals("openappstore")) {
+      result.success("Android " + android.os.Build.VERSION.RELEASE);	      String android_id = call.argument("android_id");
+      try {
+        mRegistrar.activity().startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + android_id)));
+      } catch (android.content.ActivityNotFoundException e) {
+        mRegistrar.activity().startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=" + android_id)));
+      }
+      result.success(null);
+    }
+    else {
       result.notImplemented();
     }
   }
