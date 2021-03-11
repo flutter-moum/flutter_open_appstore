a61f6aa6e39cd0adc8558ba45e6428ab7cf95659
ChangJoo Park(박창주) <pcjpcj2@gmail.com>
2021-03-11 17:00:26 +0900

    Migrate to Flutter null safety

--- lib/open_appstore.dart
+++ lib/open_appstore.dart
@@ -6,12 +6,12 @@ class OpenAppstore {
   static const MethodChannel _channel =
       const MethodChannel('flutter.moum.open_appstore');
 
-  static Future<String> get platformVersion async {
-    final String version = await _channel.invokeMethod('getPlatformVersion');
+  static Future<String?> get platformVersion async {
+    final String? version = await _channel.invokeMethod('getPlatformVersion');
     return version;
   }
 
-  static void launch({String androidAppId, String iOSAppId}) async {
+  static void launch({String? androidAppId, String? iOSAppId}) async {
     await _channel.invokeMethod(
         'openappstore', {'android_id': androidAppId, 'ios_id':iOSAppId});
   }
